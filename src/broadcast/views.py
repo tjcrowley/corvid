7fb4c2b10c55af854da6cbcef5be656328139fb5
Darren Mckeeman <darren@kinkbnb.com>
2017-08-12 14:45:36 -0700

    Added video player code

--- src/broadcast/views.py
+++ src/broadcast/views.py
@@ -35,3 +35,46 @@ class ChannelListView(ListView):
     model = Channel
     
 
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.views.decorators.http import require_POST
+
+from .models import Stream
+
+
+@require_POST
+def on_publish(request):
+    # nginx-rtmp makes the stream name available in the POST body via `name`
+    stream_key = request.POST['name']
+
+    # Assuming we have a model `Stream` with a foreign key
+    # to `django.contrib.auth.models.User`, we can
+    # lookup the stream and verify the publisher is allowed to stream.
+    stream = get_object_or_404(Channel, key=stream_key)
+
+    # You can ban streamers by setting them inactive.
+    #if not stream.user.is_active:
+    #    return HttpResponseForbidden("inactive user")
+
+    # Set the stream live
+    stream.live_at = timezone.now()
+    stream.save()
+
+    # Redirect the private stream key to the user's public stream
+    # NOTE: a relative redirect like this will not work in
+    #       Django <= 1.8
+    return HttpResponseRedirect(stream.user.username)
+
+
+@require_POST
+def on_publish_done(request):
+    # When a stream stops nginx-rtmp will still dispatch callbacks
+    # using the original stream key, not the redirected stream name.
+    stream_key = request.POST['name']
+
+    # Set the stream offline
+    Stream.objects.filter(key=stream_key).update(live_at=None)
+
+    # Response is ignored.
+    return HttpResponse("OK")
