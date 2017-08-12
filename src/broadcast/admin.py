b7b5a32dcd5731aed6a1b68108a0ca1843463a0e
Darren Mckeeman <darren@kinkbnb.com>
2017-08-12 15:12:42 -0700

    Admin stuff

--- src/broadcast/admin.py
+++ src/broadcast/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from models import Channel
+from broadcast.models import Channel
 
-class ChannelAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Channel)
