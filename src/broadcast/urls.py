a67013d240516532cc2751adc8b12cdc235e8ccb
Darren Mckeeman <darren@kinkbnb.com>
2017-08-14 06:40:17 -0700

    Added HLS endpoint handlers

--- src/broadcast/urls.py
+++ src/broadcast/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from views import new_channel, ChannelListView
+from broadcast.views import on_publish, on_publish_done
 
 urlpatterns = [
     url(r'^$', ChannelListView.as_view(), name='channel-list'),
     url(r'^add/', new_channel, name = 'new_channel'),
+    url(r'^on_publish/', on_publish, name = 'on_publish'),
+    url(r'^on_publish_done/', on_publish_done, name = 'on_publish_done'),
     ]
