71b2f24be87e70a74ea89e7c95f50f66c586e305
Darren Mckeeman <darren@kinkbnb.com>
2017-08-24 08:06:34 -0700

    Added permissioons

--- src/broadcast/models.py
+++ src/broadcast/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 #models.py
 import os
 from django.utils.text import slugify
-from string import count
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 def get_image_path(instance, filename):
     return os.path.join('photos', str(instance.id), filename)
@@ -53,3 +55,19 @@ class ChannelDomain(models.Model):
 class ChannelUser(models.Model):
     channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- src/layout/views.py
+++ src/layout/views.py
@@ -3,14 +3,16 @@
 from django.shortcuts import render, render_to_response, HttpResponseRedirect,\
     get_object_or_404
 import django
-from broadcast.models import Channel
+from broadcast.models import Channel, ChannelMod, ChannelUser
 
 def home(request):
     """ Default view for the root """
     djangoversion = django.get_version()
     if request.subdomain:
         channel = get_object_or_404(Channel,slug=request.subdomain)
-        return render(request, 'broadcast/channel.html',{'djangoversion':djangoversion,'channel':channel })
+        channelmod = ChannelMod(user=request.user, channel = channel)
+        channeluser = ChannelUser(user=request.user, channel=channel)        
+        return render(request, 'broadcast/channel.html',{'djangoversion':djangoversion,'channel':channel ,'channelmod':channelmod ,'channeluser':channeluser })
     else:
         return render(request, 'layout/home.html',{'djangoversion':djangoversion })
 
