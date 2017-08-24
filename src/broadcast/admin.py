aa93edbd34e5a9793dc96f2acbbdbeb7cf0005d1
Darren Mckeeman <darren@kinkbnb.com>
2017-08-24 09:02:32 -0700

    Added objects in admin

--- src/broadcast/admin.py
+++ src/broadcast/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from broadcast.models import Channel
+from broadcast.models import Channel, ChannelUser, ChannelMod
 
 admin.site.register(Channel)
+
+admin.site.register(ChannelMod)
+
+admin.site.register(ChannelUser)
