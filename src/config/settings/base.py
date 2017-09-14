867127cccd05079bb1be0c635264d46e822ce4b7
Darren Mckeeman <darren@kinkbnb.com>
2017-09-14 11:30:27 -0700

    Changed to email stuff

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -301,8 +301,8 @@ INTERNAL_IPS = ('127.0.0.1')
 # Set this to true if you use a proxy that sets X-Forwarded-Host
 #USE_X_FORWARDED_HOST = False
 
-SERVER_EMAIL = "webmaster@example.com"
-DEFAULT_FROM_EMAIL = "webmaster@example.com"
+SERVER_EMAIL = "support@theavsociety.com"
+DEFAULT_FROM_EMAIL = "support@theavsociety.com"
 SYSTEM_EMAIL_PREFIX = ""
 
 ## Log settings
