eca5deaf2ae23af7ced89d9d132be0838ca80e3e
Darren Mckeeman <darren@kinkbnb.com>
2017-10-23 06:41:07 -0700

    Channel initial form

--- src/broadcast/views.py
+++ src/broadcast/views.py
@@ -62,7 +62,7 @@ def edit_channel(request):
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = ChannelForm()
+        form = ChannelForm(initial=channel)
 
     return render(request, 'forms/form.html', {'form': form})
 
